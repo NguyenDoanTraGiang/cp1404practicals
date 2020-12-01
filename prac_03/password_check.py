cf3f2d1236a974dfd9562fecc5e1897009272d4e
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-01 22:19:30 +0800

    Refactor password check program into functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,25 @@
-password = input("Enter your password: ")
+def main():
+    password = get_password()
 
-in_length = False
-while not in_length:
-    if len(password) < 5:
-        print("Minimum password length is 5 characters.")
-        password = input("Enter your password: ")
-    else:
-        in_length = True
+    display_asterisks(password)
 
-for char in password:
-    print("*", end='')
+
+def display_asterisks(password):
+    for character in password:
+        print("*", end='')
+
+
+def get_password():
+    password = input("Enter your password: ")
+    in_length = False
+    while not in_length:
+        if len(password) < 5:
+            print("Minimum password length is 5 characters.")
+            password = input("Enter your password: ")
+        else:
+            in_length = True
+    return password
+
+
+if __name__ == '__main__':
+    main()
