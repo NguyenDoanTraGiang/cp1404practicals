955e2a3a9f792a762de76819fe0d9559dc3f8468
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-08 22:13:36 +0800

    Add question 2 program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,5 +7,13 @@ print("The first number is", numbers[0])
 print("The last number is", numbers[-1])
 print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
-print("The average of the number is", sum(numbers)/len(numbers))
+print("The average of the number is", sum(numbers) / len(numbers))
 
+# Question 2:
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name_input = input("\nEnter your name: ")
+if name_input in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
