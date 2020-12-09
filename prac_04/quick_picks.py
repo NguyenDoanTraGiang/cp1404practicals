1dd26b89b5d8311f521bc7b83dc1ce09d02bd6b1
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-09 15:42:35 +0800

    Add quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,4 +5,12 @@ MAX_RANGE = 45
 MIN_RANGE = 1
 NUMBER_EACH_LINE = 6
 
-
+quick_pick_line = int(input("How many quick picks? "))
+for line in range(0, quick_pick_line):
+    numbers = []
+    for num in range(0, NUMBER_EACH_LINE):
+        number = random.randint(1, 45)
+        while number in numbers:
+            number = random.randint(1, 45)
+        numbers.append(number)
+    print(numbers)
