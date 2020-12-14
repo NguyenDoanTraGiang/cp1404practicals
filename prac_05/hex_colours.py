857854dc335e8d9112f8d0fc1913b77955a409a5
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-14 22:30:35 +0800

    Add dictionary of colours and their codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+COLOUR_TO_CODE = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "Beige": "#f5f5dc", "Black": "#000000",
+          "BlanchedAlmond": "#ffebcd", "BlueViolet": "#8a2be2", "Brown": "#a52a2a", "CadetBlue": "#5f9ea0",
+          "Coral": "#ff7f50", "CornflowerBlue": "#6495ed", "DarkGreen": "#006400", "DarkOrange": "#ff8c00"}
+print(COLOUR_TO_CODE)
+
