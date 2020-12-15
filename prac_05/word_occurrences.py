33ff5b79645f41ebc47368ade079ab4d5de2f25e
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-15 10:46:40 +0800

    Align the words and counts

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,10 +1,24 @@
 word_to_count = {}
 sentence = input("Text: ")
 words = sentence.split(" ")
-print(words)
+words.sort()
+
+"""Count occurrence of words"""
 for word in words:
     if word in word_to_count:
         word_to_count[word] += 1
     else:
         word_to_count[word] = 1
-print(word_to_count)
+
+ """Finding the longest word"""
+longest_length = 0
+for word in words:
+    length = len(word)
+    if len(word) > longest_length:
+        longest_length = len(word)
+    else:
+        longest_length = longest_length
+
+"""Display the count of the words"""
+for word, count in word_to_count.items():
+    print("{:{}} : {}".format(word, longest_length, count))
