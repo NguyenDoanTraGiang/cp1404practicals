d52517584f0c36aada6ee177e35e9f1fe9847ed2
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-16 09:07:23 +0800

    Change first if-else loop

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,23 +7,24 @@ def main():
 
         if email == "":
             enter_blank = True
-        else:
             for email, name in email_to_name.items():
-                print("{} ({})".format(email_to_name[name], email))
+                print("{} ({})".format(email_to_name[email], email))
+        else:
+            username = email.split("@")
 
-        username = email.split("@")
+            name = username[0].split(".")
+            complete_name = " ".join(name).title()
 
-        name = username[0].split(".")
-        complete_name = " ".join(name).title()
+            email_to_name[email] = complete_name
 
-        email_to_name[email] = complete_name
-
-        confirm_name = input("Is your name {}? (Y/n) ".format(email_to_name[email])).lower()
-        if confirm_name == "" or confirm_name in "yes":
-            continue
-        elif confirm_name in "no":
-            new_name = input("Name: ").title()
-            email_to_name[email] = new_name
+            confirm_name = input("Is your name {}? (Y/n) ".format(email_to_name[email])).lower()
+            if confirm_name == "" or confirm_name in "yes":
+                continue
+            elif confirm_name in "no":
+                new_name = input("Name: ").title()
+                email_to_name[email] = new_name
+            else:
+                print("Invalid answer")
 
 
 if __name__ == '__main__':
