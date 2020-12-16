bb2a10928f8fcd2703abc01509b5873484479803
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-16 09:08:47 +0800

    refactor to get name from email function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,12 +10,7 @@ def main():
             for email, name in email_to_name.items():
                 print("{} ({})".format(email_to_name[email], email))
         else:
-            username = email.split("@")
-
-            name = username[0].split(".")
-            complete_name = " ".join(name).title()
-
-            email_to_name[email] = complete_name
+            get_name_from_email(email, email_to_name)
 
             confirm_name = input("Is your name {}? (Y/n) ".format(email_to_name[email])).lower()
             if confirm_name == "" or confirm_name in "yes":
@@ -25,6 +20,13 @@ def main():
                 email_to_name[email] = new_name
             else:
                 print("Invalid answer")
+
+
+def get_name_from_email(email, email_to_name):
+    username = email.split("@")
+    name = username[0].split(".")
+    complete_name = " ".join(name).title()
+    email_to_name[email] = complete_name
 
 
 if __name__ == '__main__':
