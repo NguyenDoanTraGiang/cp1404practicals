486f6078939c87078215ae7b411eb58033634919
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-16 08:22:03 +0800

    Add program to get name from email address

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,1 +1,12 @@
+def main():
+    email_to_name = {}
 
+    email = input("Email: ")
+
+    username = email.split("@")
+
+    name = username[0].split(".")
+    complete_name = " ".join(name).title()
+
+if __name__ == '__main__':
+    main()
