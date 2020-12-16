b2131a38d5f7376be2acfe36ebb2fce85574fdac
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-16 08:29:37 +0800

    Add while loop

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,14 +1,16 @@
 def main():
     email_to_name = {}
+    enter_blank = False
+    while not enter_blank:
+        email = input("Email: ")
 
-    email = input("Email: ")
+        username = email.split("@")
 
-    username = email.split("@")
+        name = username[0].split(".")
+        complete_name = " ".join(name).title()
 
-    name = username[0].split(".")
-    complete_name = " ".join(name).title()
+        email_to_name[email] = complete_name
 
-    email_to_name[email] = complete_name
 
 
 if __name__ == '__main__':
