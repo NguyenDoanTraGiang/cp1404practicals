10afc50531e6e51dd6466bf58f82480424ebfdfe
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-21 22:36:42 +0800

    Add __str__ method to class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -10,3 +10,6 @@ class ProgrammingLanguage:
             return True
         else:
             return False
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
