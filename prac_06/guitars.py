71566b6d4cc46d6eba8444dd63cfc8ab1ed4536c
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-22 21:54:58 +0800

    Add user input

---
+++ prac_06/guitars.py
@@ -0,0 +1,13 @@
+from prac_06.guitar import Guitar
+print("My guitars!")
+enter_blank_name = False
+
+while not enter_blank_name:
+    name = input("Name: ")
+
+    if name == "":
+        break
+
+    year = int(input("Year: "))
+    cost = int(input("Cost: "))
+
