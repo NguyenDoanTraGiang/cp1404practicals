aa27fd48e007823a675201c6ee3deb523a546ba1
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-22 18:43:35 +0800

    Print languages with dynamic typing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -8,7 +8,9 @@ print(ruby)
 print(python)
 print(visual_basic)
 
-class_objects = [ProgrammingLanguage.object_list(ruby), ProgrammingLanguage.object_list(python), ProgrammingLanguage.object_list(visual_basic)]
-print(class_objects)
-
-
+class_objects = [ProgrammingLanguage.object_list(ruby), ProgrammingLanguage.object_list(python),
+                 ProgrammingLanguage.object_list(visual_basic)]
+print("\nThe dynamically typed languages are:")
+for class_object in class_objects:
+    if class_object[2] is True:
+        print(class_object[0])
