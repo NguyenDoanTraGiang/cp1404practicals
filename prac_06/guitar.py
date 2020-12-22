734d24772dae70666d168b775fcf32500f1b2156
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-22 21:26:53 +0800

    Modify string formatting - two decimal places

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -5,7 +5,7 @@ class Guitar:
         self.cost = cost
 
     def __str__(self):
-        return "{} ({}) : ${}".format(self.name, self.year, self.cost)
+        return "{} ({}) : ${:.2f}".format(self.name, self.year, self.cost)
 
     def get_age(self):
         age = 2020 - self.year
