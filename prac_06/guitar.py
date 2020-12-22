96b7d550704af6b979cd7b9e824e1bf2fc6ff8ab
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2020-12-22 21:20:23 +0800

    Add is_vintage() method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,3 +10,9 @@ class Guitar:
     def get_age(self):
         age = 2020 - self.year
         return age
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
