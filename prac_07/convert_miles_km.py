ebfcec1af34fd8d8c9b0cfc5c387b7d592522b5c
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-03 16:15:39 +0800

    Add convert functionality and StringProperty

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,12 +1,22 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.app import StringProperty
 
 
 class MilesToKilometersApp(App):
+    default = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def convert_mi_km(self, miles):
+        kilometers = miles * 1.609
+        self.root.ids.output_label.text = str(kilometers)
+
+    def handle_convert(self):
+        self.default = self.root.ids.input_number.text
 
 
 MilesToKilometersApp().run()
