5ca95986ce06989cf013ec2f82e0d00f4cacf64b
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-04 21:27:59 +0800

    Remove convert button for result appear immediately

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -12,17 +12,21 @@ class MilesToKilometersApp(App):
         return self.root
 
     def convert_mi_km(self):
+        """Convert miles into kilometers"""
         miles = self.validate_miles()
         kilometers = miles * 1.609
         self.root.ids.output_label.text = str(kilometers)
 
     def handle_convert(self):
+        """Update the input field automatically"""
         self.default = self.root.ids.input_number.text
 
     def handle_increment(self, change):
+        """Change the value in the input field by change parameter"""
         self.root.ids.input_number.text = str(self.validate_miles() + change)
 
     def validate_miles(self):
+        """If input field is empty or not a number then return 0"""
         try:
             miles = float(self.root.ids.input_number.text)
             return miles
