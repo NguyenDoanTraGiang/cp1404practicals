a8372b6213306122383a881f05d97554af6ee064
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 14:00:32 +0800

    Print taxi's details and new current fare

--- prac_08/taxi_text.py
+++ prac_08/taxi_text.py
@@ -5,3 +5,5 @@ new_taxi.drive(40)
 print("Taxi's detail: {}\nTaxi's current fare: ${}".format(new_taxi, new_taxi.get_fare()))
 # Star new fare
 new_taxi.start_fare()
+new_taxi.drive(100)
+print("Taxi's detail: {}\nTaxi's current fare: ${}".format(new_taxi, new_taxi.get_fare()))
