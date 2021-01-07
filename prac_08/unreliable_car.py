d5c73569e30ad187d63362f7b113783a8b61f7a2
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 21:58:27 +0800

    Replace Car.__init__ with super().__init__

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,7 +1,8 @@
 from prac_08.car import Car
+from random import randint
 
 class UnreliableCar(Car):
     """Specialised version of Car that include a reliability percentage"""
     def __init__(self, name, fuel, reliability):
-        Car.__init__(self, name, fuel)
+        super().__init__(name, fuel)
         self.reliability = reliability
