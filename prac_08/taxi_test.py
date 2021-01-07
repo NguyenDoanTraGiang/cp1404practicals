dcc9746d2a3514c0dfecb0075958aab98984511a
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 14:16:54 +0800

    Add class variable

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,6 +1,6 @@
 from prac_08.taxi import Taxi
 
-new_taxi = Taxi("Prius 1", 100, 1.23)
+new_taxi = Taxi("Prius 1", 100)
 new_taxi.drive(40)
 print("Taxi's detail: {}\nTaxi's current fare: ${}".format(new_taxi, new_taxi.get_fare()))
 # Star new fare
