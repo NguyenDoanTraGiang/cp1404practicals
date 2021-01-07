ca2ec55c100a2d67fa14d0d1fba8b04d558d8aae
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 13:59:50 +0800

    Start new fare

--- prac_08/taxi_text.py
+++ prac_08/taxi_text.py
@@ -2,4 +2,6 @@ from prac_08.taxi import Taxi
 
 new_taxi = Taxi("Prius 1", 100, 1.23)
 new_taxi.drive(40)
-print("Taxi's detail: {}\nTaxi's current fees: ${}".format(new_taxi, new_taxi.get_fare()))
+print("Taxi's detail: {}\nTaxi's current fare: ${}".format(new_taxi, new_taxi.get_fare()))
+# Star new fare
+new_taxi.start_fare()
