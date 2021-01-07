18a40bc7bfc75483edd32f434e1c1d54b4fd2707
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 13:57:32 +0800

    Print the car details and fees

--- prac_08/taxi_text.py
+++ prac_08/taxi_text.py
@@ -1,3 +1,5 @@
 from prac_08.taxi import Taxi
 
 new_taxi = Taxi("Prius 1", 100, 1.23)
+new_taxi.drive(40)
+print("Taxi's detail: {}\nTaxi's current fees: ${}".format(new_taxi, new_taxi.get_fare()))
