b0798e61e0fe99ee86ebd4ee4e229f1652f0f23c
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-07 22:04:41 +0800

    Add drive function

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,8 +1,17 @@
 from prac_08.car import Car
 from random import randint
 
+
 class UnreliableCar(Car):
     """Specialised version of Car that include a reliability percentage"""
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Only drive if a random number < reliability"""
+        if randint(0, 100) > self.reliability:
+            distance = 0
+        # Drive the car regardless of the distance
+        driven_distance = super().drive(distance)
+        return driven_distance
