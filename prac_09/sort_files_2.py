b692a756aec7230c3c3deafd33707746beb358c7
NguyenDoanTraGiang <doantragiang.nguyen@my.jcu.edu.au>
2021-01-20 14:46:06 +0800

    Create subdirectories and move files

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -9,7 +9,7 @@ def main():
     # Print all file in current directory
     print("All file in current directory is {}".format(os.listdir('.')))
 
-    category_to_extension = {}
+    extension_to_category = {}
     mention_category = []  # List to avoid repetition
     for file_name in os.listdir('.'):
         # Ignore directories
@@ -18,12 +18,21 @@ def main():
         # Separate file name from file type
         names_and_types = file_name.split('.')
 
-        if names_and_types[1] not in mention_category:
-            # Create a dictionary with file extensions as keys
-            user_category = input("What category would you like to sort {} files into? ".format(names_and_types[1]))
-            # Add file extension into a list to avoid repetition
-            mention_category.append(names_and_types[1])
-            category_to_extension[names_and_types[1]] = user_category
+        # Create a dictionary with file extensions as keys
+        user_category = input("What category would you like to sort {} files into? ".format(names_and_types[1]))
+        # Add file extension into a list to avoid repetition
+        extension_to_category[names_and_types[1]] = user_category
+        print(extension_to_category)
+
+        try:
+            # Create subdirectories based on categories
+            os.mkdir(user_category)
+        except FileExistsError:
+            pass
+
+        # Move file based on file extension and chosen categories
+        shutil.move(file_name, user_category)
 
 
 main()
+
